bfbacd0e75fed0000aa5e8d4cff3660883f7edc1
Nionel Ibarra <nionelibarra@gmail.com>
2023-04-03 15:25:18 +0800

    deleted initial files

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAm-WAlX4fgmOXY2xFusVwm9MUpEa3A00M",
+  authDomain: "slack-clone-eb00c.firebaseapp.com",
+  projectId: "slack-clone-eb00c",
+  storageBucket: "slack-clone-eb00c.appspot.com",
+  messagingSenderId: "231254922916",
+  appId: "1:231254922916:web:b9f6283535f558c344b831"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
